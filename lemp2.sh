8d45c575a8806e6ec8c0ed0e8fe67ec5294711ac
antoniobm1 <44428903+antoniobm1@users.noreply.github.com>
2019-12-14 16:45:24 +0100

    Create lemp2.sh

---
+++ lemp2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set +x
+#actualizamos los repositorios
+apt-get update
+
+#Instalamos Nginx
+apt-get install nginx -y
+
+#Instalación de php-fpm y php-mysql
+apt-get install php-fpm php-mysql -y
+
+#Instalamos git
+apt-get install git -y
+
+#Instalamos la aplicación web
+cd /var/www/html
+rm -rf iaw-practica-lamp
+git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
+chown www-data:www-data iaw-practica-lamp * -R
+
+#Cambiamos el achivo config.php
+cd ~
+cd /var/www/html/iaw-practica-lamp/src
+sed -i "s/localhost/3.93.60.84/" config.php
+
+#Cambiamos cgi.fix_pathinfo
+cd ~
+cd /etc/php/7.2/fpm
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" php.ini
+systemctl restart php7.2-fpm
+
+# Editamos el archivo de configuración /etc/nginx/sites-available/default
+cd /home/ubuntu
+cp Practica6/default /etc/nginx/sites-available/
+systemctl restart nginx
+
+#Comprobamos
+cd /home/ubuntu
+cp practica6/info.php /var/www/html/
