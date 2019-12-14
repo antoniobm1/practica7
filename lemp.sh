68fdb34b35da414b3282bd9538babdcb3ffe2d48
antoniobm1 <44428903+antoniobm1@users.noreply.github.com>
2019-12-14 16:43:53 +0100

    Create lemp.sh

---
+++ lemp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+#actualizamos repositorios
+apt-get update
+
+# instalamos nginx
+apt-get install nginx -y
+
+#instalacion de php-fpm y php-mysql
+apt-get install php-fpm php-mysql -y
+
+#instalamos git
+apt-get install git -y
+
+#clonamos nuestro archivo de nuestro repositorio
+cd /home/ubuntu
+git clone https://github.com/ElEmEnTzxc/practica7.git
+
+#borramos el archivo default para copiar el nuevo
+cd /etc/nginx/sites-enabled/
+rm -r default
+
+#Copiamos el archivo descargado en la carpeta de nginx
+cp /home/ubuntu/practica7/default /etc/nginx/sites-enabled/
+
+#reiniciamos el servicio
+systemctl restart nginx
